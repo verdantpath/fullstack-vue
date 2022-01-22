64b1bca90b7f9081b59e4e59cebad18fa6e97f7d
verdantpath <m_dittmann@yahoo.com>
2022-01-21 19:27:26 -0800

    Add login, route guards, all done

--- routing/shopping_cart/src/app/router/index.js
+++ routing/shopping_cart/src/app/router/index.js
@@ -3,10 +3,20 @@ import CartList from '../components/cart/CartList.vue';
 import ProductList from '../components/product/ProductList.vue';
 import NotFound from '../components/NotFound.vue';
 import ProductItem from '../components/product/ProductItem.vue';
+import LoginBox from '../components/login/LoginBox.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes:[
+    {
+      path: "/login",
+      component: LoginBox,
+      beforeEnter: (to, from, next) => {
+        const token = localStorage.getItem('token');
+        if (token) next('/products');
+        else next();
+      }
+    },
     {
       path: "/products",
       component: ProductList,
@@ -19,6 +29,11 @@ const router = createRouter({
       path: "/products/:id",
       component: ProductItem,
       props: true,
+      beforeEnter: (to, from, next) => {
+        const id = to.params.id;
+        if (![1,2,3,4].includes(Number(id))) next('/not-found');
+        else next();
+      }
     },
     {
       path: "/",
@@ -29,6 +44,12 @@ const router = createRouter({
       component: NotFound
     }
   ],
+});
+
+router.beforeEach((to, from, next) => {
+  const token = localStorage.getItem('token');
+  if (!token && to.path !== '/login') next('/login');
+  else next();
 });
 
 export default router;
